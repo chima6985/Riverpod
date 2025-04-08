9f619a58af9752f8a5e6f250cf963468e3cf4151
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 10:16:48 +0100

    image 3

--- lib/providers/products_proviier.dart
+++ lib/providers/products_proviier.dart
@@ -3,4 +3,5 @@
 const List <Product> allProducts = [
   Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
   Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
 ];
