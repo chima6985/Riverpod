fee70f04c9d2cee737de172b28cff206c0fa59a4
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 10:26:31 +0100

    formated

--- lib/providers/products_proviier.dart
+++ lib/providers/products_proviier.dart
@@ -1,12 +1,44 @@
- import 'package:riverpod_files/models/product.dart';
+import 'package:riverpod_files/models/product.dart';
 
-const List <Product> allProducts = [
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'2', title: 'Karati Kit', price: 34, image: 'assets/products/karati.png'),
-  Product(id:'3', title: 'Denim Jorts', price: 54, image: 'assets/products/jeans.png'),
-  Product(id:'4', title: 'Red Backpack', price: 14, image: 'assets/products/backpack.png'),
-  Product(id:'5', title: 'Drum & Sticks', price: 29, image: 'assets/products/drum.png'),
-  Product(id:'6', title: 'Blue SuitCase', price: 44, image: 'assets/products/suitcase.png'),
-  Product(id:'7', title: 'Roller Skates', price: 52, image: 'assets/products/skates.png'),
-  Product(id:'8', title: 'Electric Guitar', price: 79, image: 'assets/products/guitar.png'),
+const List<Product> allProducts = [
+  Product(
+      id: '1',
+      title: 'Groovy Shorts',
+      price: 12,
+      image: 'assets/products/shorts.png'),
+  Product(
+      id: '2',
+      title: 'Karati Kit',
+      price: 34,
+      image: 'assets/products/karati.png'),
+  Product(
+      id: '3',
+      title: 'Denim Jorts',
+      price: 54,
+      image: 'assets/products/jeans.png'),
+  Product(
+      id: '4',
+      title: 'Red Backpack',
+      price: 14,
+      image: 'assets/products/backpack.png'),
+  Product(
+      id: '5',
+      title: 'Drum & Sticks',
+      price: 29,
+      image: 'assets/products/drum.png'),
+  Product(
+      id: '6',
+      title: 'Blue SuitCase',
+      price: 44,
+      image: 'assets/products/suitcase.png'),
+  Product(
+      id: '7',
+      title: 'Roller Skates',
+      price: 52,
+      image: 'assets/products/skates.png'),
+  Product(
+      id: '8',
+      title: 'Electric Guitar',
+      price: 79,
+      image: 'assets/products/guitar.png'),
 ];
