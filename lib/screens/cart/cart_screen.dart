a474d0e2413212c57e2f71e893674c54c80bd31e
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 12:10:39 +0100

    Using providers in statefull widget

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/products_proviier.dart';
 
-class CartScreen extends StatefulWidget {
+class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
 
   @override
-  State<CartScreen> createState() => _CartScreenState();
+  ConsumerState<CartScreen> createState() => _CartScreenState();
 }
 
-class _CartScreenState extends State<CartScreen> {
+class _CartScreenState extends ConsumerState<CartScreen> {
   bool showCoupon = true;
 
   @override
   Widget build(BuildContext context) {
+    final cartProduct = ref.watch(reducedProductsProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
