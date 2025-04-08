9cb125eb15fedb898ab55ec74d9f3c0e8f1139ae
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 10:21:20 +0100

    Prod names

--- lib/providers/products_proviier.dart
+++ lib/providers/products_proviier.dart
@@ -2,11 +2,11 @@
 
 const List <Product> allProducts = [
   Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'2', title: 'Karati Kit', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'3', title: 'Denim Jorts', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'4', title: 'Red Backpack', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'5', title: 'Drum & Sticks', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'6', title: 'Blue SuitCase', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'7', title: 'Roller Skates', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'8', title: 'Electric Guitar', price: 12, image: 'assets/products/shorts.png'),
 ];
