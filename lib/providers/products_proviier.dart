3c2a959be0faa0ab4ed62a4e06ab1226bb2072a7
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 10:22:19 +0100

    still on price change

--- lib/providers/products_proviier.dart
+++ lib/providers/products_proviier.dart
@@ -4,9 +4,9 @@ const List <Product> allProducts = [
   Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png'),
   Product(id:'2', title: 'Karati Kit', price: 34, image: 'assets/products/shorts.png'),
   Product(id:'3', title: 'Denim Jorts', price: 54, image: 'assets/products/shorts.png'),
-  Product(id:'4', title: 'Red Backpack', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'5', title: 'Drum & Sticks', price: 12, image: 'assets/products/shorts.png'),
-  Product(id:'6', title: 'Blue SuitCase', price: 12, image: 'assets/products/shorts.png'),
+  Product(id:'4', title: 'Red Backpack', price: 14, image: 'assets/products/shorts.png'),
+  Product(id:'5', title: 'Drum & Sticks', price: 29, image: 'assets/products/shorts.png'),
+  Product(id:'6', title: 'Blue SuitCase', price: 44, image: 'assets/products/shorts.png'),
   Product(id:'7', title: 'Roller Skates', price: 12, image: 'assets/products/shorts.png'),
   Product(id:'8', title: 'Electric Guitar', price: 12, image: 'assets/products/shorts.png'),
 ];
