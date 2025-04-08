ed467468017cdb14fdf9e96a1b3fe91a00aa2b24
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 11:36:44 +0100

    adding the providerScope tp the root of the projects, and consuming it in a statles widget

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_files/shared/cart_icon.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget{
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref ) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Garage Sale Products'),
