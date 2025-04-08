41e4e66504f89bf84d63154cffd8a21ee09b7f90
chima6985 <chimaokeke2006@gmail.com>
2025-04-08 10:15:47 +0100

    image 1

--- lib/providers/products_proviier.dart
+++ lib/providers/products_proviier.dart
@@ -1,5 +1,5 @@
  import 'package:riverpod_files/models/product.dart';
 
 const List <Product> allProducts = [
-
+  Product(id:'1', title: 'Groovy Shorts', price: 12, image: 'assets/products/shorts.png')
 ];
